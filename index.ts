b138a233cbd85653b720254ace0c407b472996e9
Zubair Nazir <zubair.nazir@quokkalabs.com>
2023-07-27 13:08:55 +0530

    initial dynamic link commit

---
+++ index.ts
@@ -0,0 +1,36 @@
+import axios from "axios";
+
+export const generateShareLink = async (
+  linkToEncode: string,
+  socialTitle: string,
+  socialDesc: string
+) => {
+  const url = process.env.FIREBASE_DYNAMICLINK + process.env.WEBAPIKEY;
+  const headers = { "Content-Type": "application/json" };
+  const request = {
+    dynamicLinkInfo: {
+      domainUriPrefix: process.env.DOMAIN_URI_PREFIX,
+      link: linkToEncode,
+      socialMetaTagInfo: {
+        socialTitle: socialTitle ?? "",
+        socialDescription: socialDesc ?? "",
+      },
+      androidInfo: {
+        androidPackageName: process.env.ANDROID_BUNDLE,
+      },
+      iosInfo: {
+        iosBundleId: process.env.IOS_BUNDLE,
+        //   iosAppStoreId: `${this.configService.get('IOS_APPSTORE_ID')}`,
+      },
+    },
+  };
+  try {
+    const response = await axios.post(url, request, { headers: headers });
+    const result = response.status === 200 ? response.data : null;
+    return {
+      shortLink: result.shortLink,
+    };
+  } catch (e) {
+    console.log(e);
+  }
+};
